0065be84a4c1e343a377984c665bf0ae2f9c749a
Randy Taylor <randyisbusy@gmail.com>
2019-10-22 06:58:44 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,8 @@
 class Helpers
+  def self.current_user(session) 
+     @match = User.find_by(:id == session[:user_id])
+  end 
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
